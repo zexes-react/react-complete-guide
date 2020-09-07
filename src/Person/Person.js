3fa8698732096f2ef9a2e63f57c47d4d18904c9a
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-07 09:57:14 +0100

    accessing data in <vv>DATA</vv>. closes #1

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const person = (props) =>{
-    return <p>I'm a {props.name} and I am {props.age} year old!</p>
+    return (
+        <div>
+            <p>I'm a {props.name} and I am {props.age} year old!</p>
+            <p>{props.children}</p>
+        </div>)
 }
 
 
