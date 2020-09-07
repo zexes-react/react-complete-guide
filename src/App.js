fb547a55388f22e99e11aa55fcc351962a6713de
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-07 17:16:53 +0100

    Adding two way styling. closes #6

--- src/App.js
+++ src/App.js
@@ -32,12 +32,22 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
           <p>This is really working</p>
            {/*this is inefficient, use the below*/}
-          <button onClick={() => this.switchNameHandler('Ezekiel!!#')}>Switch Name</button>
+          <button style={style}
+              onClick={() => this.switchNameHandler('Ezekiel!!#')}>Switch Name</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, 'ZIKOZEE!!')}
           changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
