096b5e118abccf281f8accd7c08fd2e45f35b34a
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-07 15:23:05 +0100

    using state and Handling Events. closes #2

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age:28 },
+      { name: 'Manu', age:29 },
+      { name: 'Stephanie', age:  26}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked!')
+    // DON'T DO THIS: this.state.persons[0].name = 'Ezekiel'
+    this.setState({persons: [
+        { name: 'Ezekiel', age:28 },
+        { name: 'Manu', age:29 },
+        { name: 'Stephanie', age:  30}
+      ]})
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
           <p>This is really working</p>
-          <Person name={"Max"} age="28"/>
-          <Person name={"Manu"} age={29}>My Hobbies: Racing</Person>
-          <Person name="Stephanie" age={26}/>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
