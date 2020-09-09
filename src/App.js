cd2ca5ae6617b2db9604cba2f1321732169c27ce
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-09 12:14:02 +0100

    List and Keys. closes #12

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Person from "./Person/Person";
 class App extends Component {
     state = {
         persons: [
-            {name: 'Max', age: 28},
-            {name: 'Manu', age: 29},
-            {name: 'Stephanie', age: 26}
+            {id: 'dasc', name: 'Max', age: 28},
+            {id: 'asce', name: 'Manu', age: 29},
+            {id: 'vwwv', name: 'Stephanie', age: 26}
         ],
         otherState: 'some other value',
         showPersons: false
@@ -50,8 +50,11 @@ class App extends Component {
             persons = (
                 <div>
                     {this.state.persons.map((person, index) => {
-                        return <Person click={() => this.deletePersonHandler(index)} name={person.name}
-                                       age={person.age}/>
+                        return <Person
+                            click={() => this.deletePersonHandler(index)}
+                            name={person.name}
+                            age={person.age}
+                            key={person.id}/>
                     })}
                 </div>)
         }
