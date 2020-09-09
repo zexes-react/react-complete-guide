5ecf8d45b353694095310407f3472044a96cc3fe
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-09 11:35:18 +0100

    Handling Dynamic Content "The Javascript Way". closes #8

--- src/App.js
+++ src/App.js
@@ -47,7 +47,19 @@ class App extends Component {
             border: '1px solid blue',
             padding: '8px',
             cursor: 'pointer'
+        }
 
+        let persons = null;
+
+        if (this.state.showPersons) {
+            persons = (
+                <div>
+                    <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                    <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+                            click={this.switchNameHandler.bind(this, 'ZIKOZEE!!')}
+                            changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+                    <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                </div>)
         }
 
         return (
@@ -58,17 +70,8 @@ class App extends Component {
                 <button style={style}
                         onClick={this.togglePersonsHandler}>Toggle Persons
                 </button>
-                {
-                    this.state.showPersons ?
-                        <div>
-                            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
-                                    click={this.switchNameHandler.bind(this, 'ZIKOZEE!!')}
-                                    changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-                            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-                        </div> : null
-                }
 
+                {persons}
             </div>
         );
         //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
