029ef65609d98d51d81e29690e50e65142ebd1c1
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-11 20:43:48 +0100

    Using Styled Components library. closes #16

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from "./Person/Person";
+
+const StyledButton = styled.button`
+    background-color: ${props => props.alt ? 'red' : 'green'};
+    color: white;
+    font: inherit;
+    border: 1px solid blue;
+    padding: 8px;
+    cursor: pointer;
+    
+    &:hover {
+        background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+        color: black;
+    }
+`;
 
 class App extends Component {
     state = {
@@ -89,21 +103,20 @@ class App extends Component {
 
 
         return (
-            <StyleRoot>
+
                 <div className="App">
                     <h1>Hi, I'm a react App</h1>
                     <p className={classes.join(' ')}>This is really working</p>
                     {/*this is inefficient, use the below*/}
-                    <button style={style}
-                            onClick={this.togglePersonsHandler}>Toggle Persons
-                    </button>
+                    <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons
+                    </StyledButton>
 
                     {persons}
                 </div>
-            </StyleRoot>
+
         );
         //   return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
     }
 }
 
-export default Radium(App);
+export default App;
