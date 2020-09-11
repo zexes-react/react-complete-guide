d806c139262ad41e6f851d5a760063cc55e0ff62
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-11 18:26:56 +0100

    Setting Styles Dynamically, setting Class Names dynamically. closes #14

--- src/App.js
+++ src/App.js
@@ -44,7 +44,8 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'white',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
@@ -64,13 +65,24 @@ class App extends Component {
                             key={person.id}
                             changed ={(event) => this.nameChangedHandler(event, person.id)}/>
                     })}
-                </div>)
+                </div>
+            )
+            style.backgroundColor = "red";
         }
+
+        let classes = [];
+        if(this.state.persons.length <= 2){
+            classes.push('red'); //classes = ['red']
+        }
+        if(this.state.persons.length <= 1){
+            classes.push('bold'); //classes = ['red', 'bold']
+        }
+
 
         return (
             <div className="App">
                 <h1>Hi, I'm a react App</h1>
-                <p>This is really working</p>
+                <p className={classes.join(' ')}>This is really working</p>
                 {/*this is inefficient, use the below*/}
                 <button style={style}
                         onClick={this.togglePersonsHandler}>Toggle Persons
