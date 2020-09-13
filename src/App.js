faba2213bc9a75d8059daf191d07fb85c638a6a1
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-13 20:44:52 +0100

    Working with CSS Modules. closes #17

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import './App.css';
+
+import classes from './App.css';
 import Person from "./Person/Person";
 import './Person/Person.css'
 
@@ -46,20 +47,8 @@ class App extends Component {
     }
 
     render() {
-        const style = {
-            backgroundColor: 'green',
-            color: 'white',
-            font: 'inherit',
-            border: '1px solid blue',
-            padding: '8px',
-            cursor: 'pointer',
-            ':hover': {
-                backgroundColor: 'lightgreen',
-                color: 'black'
-            }
-        }
-
         let persons = null;
+        let btnClass = '';
 
         if (this.state.showPersons) {
             persons = (
@@ -74,29 +63,25 @@ class App extends Component {
                     })}
                 </div>
             )
-            style.backgroundColor = "red";
-            style[':hover'] = {
-                backgroundColor: 'salmon',
-                    color: 'black'
-            }
+            btnClass = classes.Red;
         }
 
-        let classes = [];
+        let assignedClasses = [];
         if(this.state.persons.length <= 2){
-            classes.push('red'); //classes = ['red']
+            assignedClasses.push(classes.red); //classes = ['red']
         }
         if(this.state.persons.length <= 1){
-            classes.push('bold'); //classes = ['red', 'bold']
+            assignedClasses.push(classes.bold); //classes = ['red', 'bold']
         }
 
 
         return (
 
-                <div className="App">
+                <div className={classes.App}>
                     <h1>Hi, I'm a react App</h1>
-                    <p className={classes.join(' ')}>This is really working</p>
+                    <p className={assignedClasses.join(' ')}>This is really working</p>
                     {/*this is inefficient, use the below*/}
-                    <button className="button" onClick={this.togglePersonsHandler}>Toggle Persons
+                    <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons
                     </button>
 
                     {persons}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 
-// import './Person.css'
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 16px auto;
-    border:  1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+import classes from './Person.css'
 
-
-    @media (min-width: 500px){
-            width: 450px;
-    }`;
 
 const person = (props) => {
     // const style = {
@@ -23,11 +11,11 @@ const person = (props) => {
     // };
     return (
         // <div className="Person" style={style}>
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} year old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
-        </StyledDiv>)
+        </div>)
 }
 
 
