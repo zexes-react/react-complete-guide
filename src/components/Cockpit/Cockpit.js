3b3411c750aba2c05e97459a4f3ae3f79196c385
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-19 20:52:11 +0100

    using "this.props..." in class based component and props... in functional based components. closes #21

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -20,7 +20,7 @@ const cockpit = (props)=> {
 
     return (
     <div className={classes.Cockpit}>
-        <h1>Hi, I'm a react App</h1>
+        <h1>{props.title}</h1>
         <p className={assignedClasses.join(' ')}>This is really working</p>
         {/*this is inefficient, use the below*/}
         <button className={btnClass} onClick={props.clicked}>Toggle Persons
--- src/containers/App.js
+++ src/containers/App.js
@@ -60,6 +60,7 @@ class App extends Component {
 
             <div className={classes.App}>
                 <Cockpit
+                    title={this.props.appTitle}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     clicked={this.togglePersonsHandler}/>
