6294aec3a8ccebbf9655c294d53097e5f4272267
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-22 21:35:17 +0100

    Using PropTypes. closes #35

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import Auxiliary from '../../../hoc/Auxiliary';
 import withClass from "../../../hoc/withClass";
@@ -22,5 +23,11 @@ class Person extends Component  {
     }
 }
 
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
