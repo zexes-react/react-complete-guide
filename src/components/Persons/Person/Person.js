954c25423d315d2bb0c4431ad4af865c9a57210b
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-24 21:05:20 +0100

    Context Api in classses => contextType & useContext(). closes #39

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -14,9 +14,12 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
         // this.inputElement.focus();
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated)
     }
 
     render() {
@@ -27,11 +30,9 @@ class Person extends Component {
         }
         return (
             <Auxiliary>
-                <AuthContext.Consumer>
-                    {(context) =>
-                        context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>
-                    }
-                </AuthContext.Consumer>
+                
+                {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
+
                 <p key="i1" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} year old!</p>
                 <p key='i2'>{this.props.children}</p>
                 <input key='i3' type="text"
