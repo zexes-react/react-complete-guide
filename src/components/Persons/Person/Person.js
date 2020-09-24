924eb6df463f1ce97de58ceea458810f5264dabc
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2020-09-24 19:43:48 +0100

    Using Ref: Reference to pick out any JSX(HTML) element and do something on it. closes #36

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -7,6 +7,17 @@ import classes from './Person.css'
 
 
 class Person extends Component  {
+
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus();
+        this.inputElementRef.current.focus();
+    }
+
     render() {
         console.log('[Person.js] rendering...');
         const rnd = Math.random();
@@ -17,7 +28,12 @@ class Person extends Component  {
             <Auxiliary>
             <p key="i1" onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} year old!</p>
             <p key='i2'>{this.props.children}</p>
-            <input key='i3' type="text" onChange={this.props.changed} value={this.props.name}/>
+            <input key='i3' type="text"
+                   onChange={this.props.changed}
+                   value={this.props.name}
+                   // ref={(inputEl) => {this.inputElement =inputEl}}
+                    ref={this.inputElementRef}
+            />
             </Auxiliary>
         );
     }
